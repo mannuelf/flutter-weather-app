25ddc482c6322d53c6119770a2651fc93aacea42
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-24 22:01:02 +0200

    make Networking generic

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -2,16 +2,13 @@ import 'package:http/http.dart' show Client, Response;
 import 'dart:convert';
 
 class NetworkHelper {
-  NetworkHelper();
+  NetworkHelper(this.url);
 
   Client client = Client();
+  final url;
 
-  final root = 'https://api.openweathermap.org';
-  final API_KEY = 'e14fe840c1768222dbc2a366f42b8909';
-
-  Future getData(lat, lon) async {
-    Response response = await client.get(
-        Uri.parse('$root/data/2.5/weather?lat=$lat&lon=$lon&appid=$API_KEY'));
+  Future getData() async {
+    Response response = await client.get(Uri.parse(url));
     if (response.statusCode == 200) {
       String data = response.body;
       var decodedData = jsonDecode(data);
