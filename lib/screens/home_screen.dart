ad14e08a86271b8216fd6c76c9afcd1d4dc3a700
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-26 18:46:03 +0200

    new home screen start build

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,7 @@ import '../services/weather.dart';
 import '../utilities/constants.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.locationWeather})
-      : super(key: key);
+  const HomeScreen({Key? key, required this.locationWeather}) : super(key: key);
 
   // creates arguments for this class to pass data into it
   final locationWeather;
@@ -69,71 +68,11 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         constraints: const BoxConstraints.expand(),
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  FlatButton(
-                    onPressed: () async {
-                      var weatherData = await weather.getLocationWeather();
-                      updateUI(weatherData);
-                    },
-                    child: const Icon(
-                      Icons.near_me,
-                      size: 50.0,
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () async {
-                      var typedName = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return const CityScreen();
-                          },
-                        ),
-                      );
-                      if (typedName != null) {
-                        // get weather with the string
-                        var weatherData =
-                            await weather.getCityWeather(typedName);
-                        updateUI(weatherData);
-                      }
-                    },
-                    child: const Icon(
-                      Icons.location_city,
-                      size: 50.0,
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      '$temperature°',
-                      style: kTempTextStyle,
-                    ),
-                    Text(
-                      weatherIcon,
-                      style: kConditionTextStyle,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 15.0),
-                child: Text(
-                  "$weatherMessage in $city!",
-                  textAlign: TextAlign.right,
-                  style: kMessageTextStyle,
-                ),
-              ),
-            ],
+          child: Expanded(
+            flex: 1,
+            child: Column(
+              children: [],
+            ),
           ),
         ),
       ),
