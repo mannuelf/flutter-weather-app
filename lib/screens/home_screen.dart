82626009dafe4ac2d64d20a5147a1566605a2ed5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-27 09:02:49 +0200

    add styling and layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../screens/city_screen.dart';
+import 'package:weather_icons/weather_icons.dart';
 import '../services/weather.dart';
 
 import '../utilities/constants.dart';
@@ -19,8 +19,9 @@ class _HomeScreenState extends State<HomeScreen> {
   int conditionNo = 0;
   int temperature = 0;
   String city = '';
-  Widget weatherIcon = Icon(Icons.shower, size: 90.0);
+  Widget weatherIcon = Icon(WeatherIcons.refresh, size: 132.0);
   String weatherMessage = '';
+  String condition = '';
 
   @override
   void initState() {
@@ -38,8 +39,9 @@ class _HomeScreenState extends State<HomeScreen> {
       if (weatherData == null) {
         temperature = 0;
         city = '';
-        weatherIcon = Icon(Icons.close, size: 90.0);
+        weatherIcon = Icon(WeatherIcons.refresh, size: 132.0);
         weatherMessage = 'Unable to get weather';
+        condition = '';
         return;
       }
       // anything Ui related "state" must be updated
@@ -51,6 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
       double temp = weatherData['main']['temp'];
       temperature = temp.toInt();
       weatherMessage = weather.getMessage(temperature);
+      condition = weather.getWeatherConditionLabel(conditionNo);
     });
   }
 
@@ -60,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: const AssetImage('images/location_background.jpeg'),
+            image: const AssetImage('images/new-york-bg.jpeg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
@@ -70,26 +73,37 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Center(
-                    child: Row(
-                      children: [
-                        Text(
-                          '$temperature',
-                          style: kTempNumber,
-                        ),
-                        const Text(
-                          '°',
-                          style: kTempDegrees,
-                        ),
-                      ],
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '$temperature',
+                        style: kTempNumber,
+                      ),
+                      const Text(
+                        '°',
+                        style: kTempDegrees,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    city,
+                    style: kCityLabelStyle,
                   ),
                   weatherIcon,
+                  const SizedBox(
+                    height: 60.0,
+                  ),
+                  Text(
+                    condition,
+                    style: kConditionLabelStyle,
+                  )
                 ],
               ),
             ],
