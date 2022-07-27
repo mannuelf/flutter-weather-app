76be759f4e23926472439af4863fa0b486f1bf3c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-27 09:02:14 +0200

    adds text bases conditions and refactors to return icons

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 import '../services/networking.dart';
 
@@ -9,7 +10,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 var apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
 const baseUrl = 'https://api.openweathermap.org';
-const routes = 'data/2.5/weather';
+const weatherRoute = 'data/2.5/weather';
 const units = 'metric';
 var url = '';
 
@@ -20,9 +21,7 @@ class WeatherModel {
 
     final query = 'lat=${location.latitude}&lon=${location.longitude}';
 
-    url = '$baseUrl/$routes?$query&units=$units&appid=$apiKey';
-    print('>>>URL>>>');
-    print(url);
+    url = '$baseUrl/$weatherRoute?$query&units=$units&appid=$apiKey';
     NetworkHelper networkHelper = NetworkHelper(url);
     var decodedData = await networkHelper.getData();
     final weatherData = decodedData;
@@ -32,7 +31,7 @@ class WeatherModel {
 
   Future<dynamic> getCityWeather(String city) async {
     final query = 'q=$city';
-    url = '$baseUrl/$routes?$query&units=$units&appid=$apiKey';
+    url = '$baseUrl/$weatherRoute?$query&units=$units&appid=$apiKey';
 
     NetworkHelper networkHelper = NetworkHelper(url);
     var decodedData = await networkHelper.getData();
@@ -42,24 +41,42 @@ class WeatherModel {
   }
 
   Widget getWeatherIcon(int condition) {
-    print('getWeatherIcon');
-    print(condition);
     if (condition < 300) {
-      return Icon(Icons.graphic_eq, size: 90.0);
-    } else if (condition < 400) {
-      return Icon(Icons.shower, size: 90.0);
+      return const Icon(WeatherIcons.thunderstorm, size: 132.0);
+    } else if (condition < 500) {
+      return const Icon(WeatherIcons.rain_mix, size: 132.0);
     } else if (condition < 600) {
-      return Icon(Icons.shower, size: 90.0);
+      return const Icon(WeatherIcons.rain, size: 132.0);
     } else if (condition < 700) {
-      return Icon(Icons.snowing, size: 90.0);
+      return const Icon(WeatherIcons.snowflake_cold, size: 132.0);
     } else if (condition < 800) {
-      return Icon(Icons.foggy, size: 90.0);
+      return const Icon(WeatherIcons.day_haze, size: 132.0);
     } else if (condition == 800) {
-      return Icon(Icons.sunny, size: 90.0);
+      return const Icon(WeatherIcons.day_sunny, size: 132.0);
     } else if (condition <= 804) {
-      return Icon(Icons.cloud, size: 90.0);
+      return const Icon(WeatherIcons.day_cloudy, size: 132.0);
     } else {
-      return Icon(Icons.cancel, size: 90.0);
+      return const Icon(WeatherIcons.refresh, size: 132.0);
+    }
+  }
+
+  String getWeatherConditionLabel(int condition) {
+    if (condition < 300) {
+      return 'Thunderstorm';
+    } else if (condition < 500) {
+      return 'Drizzle';
+    } else if (condition < 600) {
+      return 'Raining';
+    } else if (condition < 700) {
+      return 'Light snow';
+    } else if (condition < 800) {
+      return 'Mist | Haze | Dust';
+    } else if (condition == 800) {
+      return 'Sunny';
+    } else if (condition <= 804) {
+      return 'Few clouds';
+    } else {
+      return 'Refresh';
     }
   }
 
