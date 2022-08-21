9e3152b016c495d3409989bd37f079293bdc17c2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-08-21 07:41:21 +0200

    clean up docs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:whatsweatherdoing/models/environment.dart';
 
